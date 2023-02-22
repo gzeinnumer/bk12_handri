b8f10676d682da8aaa6d44c5dfe593562d2732d3
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2023-02-22 21:47:18 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//0 1 2 3 4 5 6 7 8 9 10
+for ($i = 0; $i < 10; $i++) {
+
+    ///
+    $no = $i + 1;
+    echo "<br>$no-ZEIN";
+
+    ///
+    if ($i % 2 == 0) {
+        echo "-Genap";
+    } else {
+        echo "-Ganjil";
+    }
+}
+
+//if(){}
+
+//for(iii){}
